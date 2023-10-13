37414cda4a1c9ec91c472653f030c14f27269323
Cao Nguyen Duong Nguyen <ngca1011@h-ka.de>
2023-10-13 14:32:29 +0200

    first commit

---
+++ solarproject/client.js
@@ -0,0 +1,36 @@
+const mqtt = require('mqtt');
+const axios = require('axios');
+const fs = require('fs');
+
+const client = mqtt.connect('mqtt://192.168.0.192:1883'); 
+
+client.on('connect', () => {
+    console.log('Client ist mit Broker verbunden');
+    
+    const request = {
+      straße: 'Augartenstraße 2',
+      land: 'Germany',
+      postleitzahl: '76137',
+      solarLeistung : 10000,
+    };
+    //Client veröffentlicht das Topic REQUEST
+    client.publish('REQUEST', JSON.stringify(request));
+});
+  
+// Client abonniert das Topic RESPONSE von Server
+client.subscribe('RESPONSE'); 
+  
+client.on('message', (topic, message) => {
+    if (topic === 'RESPONSE') {
+      const vorhersage = JSON.parse(message.toString());
+      console.log('Vorhersage der Solarproduktion:' + '\n', vorhersage);
+
+      //Protokollieren die Vorhersage für heute
+      const today = new Date();
+
+      fs.appendFile('protokollierung.txt', today + '\n' + JSON.stringify(vorhersage) + '\n', function (error) {
+        if (error) throw error;
+        console.log('Updated for ' + today);
+      });
+    }
+});
---
+++ solarproject/server.js
@@ -0,0 +1,41 @@
+const mqtt = require('mqtt');
+const axios = require('axios');
+const NodeGeocoder = require('node-geocoder');
+
+const server = mqtt.connect('mqtt://192.168.0.192:1883');
+
+const options = {
+  provider: 'google',
+  apiKey: "AIzaSyAS89DviyFBIa5dMYBfsd3Dn6mvXClxefU", 
+};
+
+const geocoder = NodeGeocoder(options);
+
+server.on('connect', () => {
+  console.log('Server ist mit Broker verbunden');
+  server.subscribe('REQUEST'); // Server aboniert das Topic solar/request von Client
+});
+
+server.on('message', async (topic, message) => {
+  if (topic === 'REQUEST') {
+    const request = JSON.parse(message.toString());
+    console.log('Anfrage wird erhalten');
+
+    requestAdresse = {address: request.straße, country: request.land, zipcode: request.postleitzahl};
+
+    //Konvertieren von Adresse zur Koordination
+    const location = await geocoder.geocode(requestAdresse);
+
+    // Rufen die Solarvorhersage von einer RESTful-API auf 
+    try {
+      const response = await axios.get(`https://api.forecast.solar/estimate/${location[0].latitude}/${location[0].longitude}/0/0/${request.solarLeistung}`);
+
+      const vorhersage = response.data;
+
+      // Server sendet die Vorhersage zurück an den Client
+      server.publish('RESPONSE', JSON.stringify(vorhersage));
+    } catch (error) {
+      console.error('Vorhersageabfrage fällt aus', error);
+    }
+  }
+});
